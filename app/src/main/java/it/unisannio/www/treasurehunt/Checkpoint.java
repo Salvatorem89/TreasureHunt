74c79b55f931bbdf24a13ef2f53872f0759324c2
salvatorem89 <salvatoremerolese89@gmail.com>
2018-09-28 16:55:06 +0200

    CreateChallenge creation

---
+++ app/src/main/java/it/unisannio/www/treasurehunt/Checkpoint.java
@@ -0,0 +1,72 @@
+package it.unisannio.www.treasurehunt;
+
+public class Checkpoint {
+    private int idCheckpoint, idRun;
+    private double latitude, longitude;
+    private String question, answer;
+
+    public Checkpoint(int idCheckpoint, int idRun, double latitude, double longitude, String question, String answer) {
+
+        this.idCheckpoint = idCheckpoint;
+        this.idRun = idRun;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public Checkpoint(){
+
+    }
+
+    public int getIdCheckpoint() {
+        return idCheckpoint;
+    }
+
+    public void setIdCheckpoint(int idCheckpoint) {
+        this.idCheckpoint = idCheckpoint;
+    }
+
+    public int getIdRun() {
+        return idRun;
+    }
+
+    public void setIdRun(int idRun) {
+        this.idRun = idRun;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+
+}
+
--- app/src/main/java/it/unisannio/www/treasurehunt/MainActivity.java
+++ app/src/main/java/it/unisannio/www/treasurehunt/MainActivity.java
@@ -1,15 +1,24 @@
 package it.unisannio.www.treasurehunt;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "CreateChallenge";
+    private static final int ERROR_DIALOG_REQUEST =9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +29,51 @@ public class MainActivity extends AppCompatActivity {
             Toast to = Toast.makeText(getApplicationContext(), "Tentativo di connessione fallito. Attiva connessione dati", Toast.LENGTH_LONG);
             to.show();
         }
+        if(isServicesOK()){
+            init();
+        }
+    }
+
+    private void init(){
+        Button btnMap = findViewById(R.id.create);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateChallenge.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 
     public void register(View view){
         startActivity(new Intent("android.intent.action.Registration"));
     }
+
     public void login(View view){
         startActivity(new Intent("android.intent.action.Login"));
     }
+
     private boolean isNetworkAvailable(){
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = cm.getActiveNetworkInfo();
